358d75c0e7f465f17a9a90aad40e29b53de0d913
Heman Gandhi <hemangandhi@gmail.com>
2017-11-11 19:00:53 -0500

    tree building for ledger done

--- flaskr/arky_utils.py
+++ flaskr/arky_utils.py
@@ -1,20 +1,71 @@
 from arky import api
 from arky import core
+import json
 
 api.use("rchain")
 
-def get_threads(root):
+def all_threads():
+    """
+    Returns every thread in the ledger.
+    """
+    roots = []
+    seen = {}
+    trans = api.get("/api/transactions")
+    if not trans.success:
+        print(trans)
+        return None
+    for post_transaction in trans.transactions:
+        if 'vendorField' not in post_transaction:
+            continue
+        post = json.loads(post_transaction['vendorField'])
+        post['kids'] = []
+        if len(seen) == 0:
+            roots.append(post)
+            seen[post['id']] = post
+        elif post['id'] in seen:
+            seen[post['id']]['votes'] = post['votes']
+        elif post['parent'] in seen:
+            seen[post['parent']]['kids'].append(post)
+        else:
+            seen[post['id']] = post
+            roots.append(post)
+    return roots
+
+
+def get_threads(root=None):
     """
     Traverses the ledge and gets everything that's
     a successor node of the root thread (basically
     treating everything as a tree).
 
-    Returns a list of dictionaries.
+    Returns the root post with kids as the direct responses.
     """
-    root_id = root.our_id
-    for post_transaction in api.get("/api/transactions"):
-        post = post_transaction.vendorField
-        #work with it!
+    if root is not None:
+        root = json.loads(root.vendorField)
+        root['kids'] = []
+        seen_ids = {root_id: root['id']}
+    else:
+        seen_ids = {}
+    trans = api.get("/api/transactions")
+    if not trans.success:
+        print(trans)
+        return None
+    for post_transaction in trans.transactions:
+        if 'vendorField' not in post_transaction:
+            continue
+        post = json.loads(post_transaction['vendorField'])
+        if len(seen_ids) == 0:
+            seen_ids[post['id']] = post
+            root = post
+            post['kids'] = []
+        elif post is None: continue
+        elif post['id'] in seen_ids:
+            seen_ids[post['id']]['votes'] = post['votes']
+        elif post['parent'] in seen_ids:
+            post['kids'] = []
+            seen_ids[post['parent']]['kids'].append(post)
+            seen_ids[post['id']] = post
+    return root
 
 def make_user(password):
     """
@@ -46,4 +97,4 @@ def put_post(post, passpharse):
     tx = core.Transaction(vendorField=post, secret=passpharse)
     api.sendTx(tx)
 
-put_post("derp", "rabbit")
+print(all_threads())
--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,6 +1,5 @@
 #port 5000 by default
 import os
-import sqlite3
 import json
 import arky_utils
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
