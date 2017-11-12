4554ec8a196eb090e537c4f66aed65ff64c45690
Heman Gandhi <hemangandhi@gmail.com>
2017-11-11 19:44:52 -0500

    small tweaks

--- RChain/rcHAIN.py
+++ RChain/rcHAIN.py
@@ -11,9 +11,9 @@ def post_to_HTML(post, wrapping = "<li>", kids_wrapping = "<ol>"):
         return wrapping[0] + '/' + wrapping[1:]
 
     acc = post['content']
-    acc += "<br/>This post has " + str(post['votes']) + "votes"
-    acc += ' <a class="upvote" data-id="' + str(post['id']) + '">upvote</a>"'
-    acc += ' <a class="downvote" data-id="' + str(post['id']) + '">downvote</a>"'
+    acc += "<br/>This post has " + str(post['votes']) + " votes"
+    acc += ' <a class="upvote" href="#" data-id="' + str(post['id']) + '">upvote</a>'
+    acc += ' <a class="downvote" href="#" data-id="' + str(post['id']) + '">downvote</a>'
     acc += kids_wrapping + ''.join(post_to_HTML(k, wrapping, kids_wrapping) for k in post['kids']) + close(kids_wrapping)
     return wrapping + acc + close(wrapping)
 
